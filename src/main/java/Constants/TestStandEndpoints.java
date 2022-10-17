0b9daf8ae5ac643980d744de07f99ada3bd0a767
Aleksey Tester <akolyaev@bk.ru>
2022-10-16 21:29:49 -0700

    Add TestStandEndpoints

---
+++ src/main/java/Constants/TestStandEndpoints.java
@@ -0,0 +1,15 @@
+package Constants;
+
+public class TestStandEndpoints {
+    public static final String BASE_URL = "https://stellarburgers.nomoreparties.site";
+    public static final String REGISTER = "/api/auth/register";
+    public static final String LOGIN = "/api/auth/login";
+    public static final String LOGOUT = "/api/auth/logout";
+    public static final String USER_DATA = "/api/auth/user";
+    public static final String TOKEN_UPDATE = "/api/auth/token";
+    public static final String PASSWORD_RESET_EMAIL = "/api/password-reset";
+    public static final String PASSWORD_RESET = "/api/password-reset/reset";
+    public static final String ORDERS_ALL = "/api/orders/all";
+    public static final String ORDER = "/api/orders";
+    public static final String INGREDIENTS = "/api/ingredients";
+}
